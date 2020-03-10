3fa328a0f0abc7664635c63a62d31e5b3a4237ca
Tomasz <tomek.dziopa@gmail.com>
2020-03-10 18:36:42 +0100

    image_path to plural

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from functions import *
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("image_path")
+    parser.add_argument("images_path")
     arguments = parser.parse_args()
-    run_classification(images_path=arguments.image_path)
+    run_classification(images_path=arguments.images_path)
 
 if __name__ == "__main__":
     main()
